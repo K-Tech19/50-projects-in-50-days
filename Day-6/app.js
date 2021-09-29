977bc37675e97839bc3178b72df0c792802781b5
K-tech19 <kennyjean561@gmail.com>
2021-09-29 12:07:45 -0400

    Day-6 complete

--- Day-6/app.js
+++ Day-6/app.js
@@ -1,1 +1,23 @@
 console.log('Scroll Animation 🚀')
+
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+  // window.innerHeight
+  const triggerBotton = window.innerHeight / 5 * 4
+
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top
+
+    if(boxTop < triggerBotton) {
+      box.classList.add('show')
+    } else {
+      box.classList.remove('show')
+    }
+
+  })
+}
