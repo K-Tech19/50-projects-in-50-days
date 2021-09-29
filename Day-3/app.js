5f5e744d27d84482e7769c00e7b3cbf16914991c
K-tech19 <kennyjean561@gmail.com>
2021-09-29 09:25:11 -0400

    Day change

--- Day-3/app.js
+++ Day-3/app.js
@@ -1,57 +1,30 @@
-const progress = document.getElementById('progress');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const circles = document.querySelectorAll('.circle');
+console.log('Project Starter 🤖 🤖 🤖');
 
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
+const colorChange = document.querySelector('.circle');
 
-let currentActive = 1
-
-next.addEventListener('click', ()=>{
-    currentActive++
-
-    if(currentActive > circles.length){
-        currentActive = circles.length
-    }
-
-    update()
-    // console.log(currentActive)
+colorChange.addEventListener('click', function cirRandomColor(){
+    const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    colorChange.style.backgroundColor = randomColor
+    console.log('test change here')
+    // return randomColor;
 })
 
 
-prev.addEventListener('click', ()=>{
-    currentActive--
 
-    if(currentActive < 1){
-        currentActive = 1;
-    }
-
-    update()
-})
-
-function update(){
-    circles.forEach((circle, idx)=>{
-        if(idx < currentActive){
-
-            circle.classList.add('active')
-        } else {
-            circle.classList.remove('active')
-        }
-    })
-
-    // get active circles
-    const actives = document.querySelectorAll('.active');
+open.addEventListener('click', () => container.classList.add('show-nav'));
+close.addEventListener('click', () => container.classList.remove('show-nav'));
 
 
-    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
 
+// function generateRandomColor(){
+//     const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+//     return randomColor;
+// }
+ 
 
-    if(currentActive === 1) {
-        prev.disabled = true
-    } else if (currentActive === circles.length){
-        next.disabled = true
-    } else {
-        prev.disabled = false
-        next.disabled = false
-    }
-}
+// document.body.style.backgroundColor = generateRandomColor();
+
 
